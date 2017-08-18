58044e250e1917b54810af31a0449c3467da8ae3
shirleycarolineteresa <30840563+shirleycarolineteresa@users.noreply.github.com>
2017-08-18 16:14:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article_one', function(req,res){
-    
-res.send('article one requested and will be served here');
+res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));    
+
 })
 app.get('/article_3', function(req,res){
     
