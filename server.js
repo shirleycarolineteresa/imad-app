6d566b2c185bca86bb18a85934562d96f8624f8e
shirleycarolineteresa <30840563+shirleycarolineteresa@users.noreply.github.com>
2017-08-18 16:48:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 })
 app.get('/article_3', function(req,res){
     
-es.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 })
 
 app.get('/article_2', function(req,res){
     
-es.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 })
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
